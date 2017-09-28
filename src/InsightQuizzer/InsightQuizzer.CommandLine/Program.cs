786ef11c632635d0b7f48d6acb5a1534b90d52bc
Michael Schnyder <michael.schnyder@zuehlke.com>
2017-09-28 20:19:04 +0200

    cleanup

--- src/InsightQuizzer/InsightQuizzer.CommandLine/Program.cs
+++ src/InsightQuizzer/InsightQuizzer.CommandLine/Program.cs
@@ -1,9 +1,4 @@
 ﻿using InsightQuizzer.Core;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using log4net.Config;
 
 namespace InsightQuizzer.CommandLine
