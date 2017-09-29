3b4fefcf79f418e75d1cf9bffbcc8fe12b82dee0
Oliver Zürcher <oliver.zuercher@gmail.com>
2017-09-29 09:06:03 +0200

    wip

---
+++ src/InsightQuizzer/InsightQuizzer.JobServer/Jobs/QuizJob.cs
@@ -0,0 +1,14 @@
+﻿using InsightQuizzer.Core;
+
+namespace InsightQuizzer.JobServer.Jobs
+{
+    public class QuizJob
+    {
+        public void Run()
+        {
+            var solver = new InsightEmployeeQuizSolver();
+
+            solver.Start();
+        }
+    }
+}
---
+++ src/InsightQuizzer/InsightQuizzer.JobServer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Jobbr.Server.Builder;
+using Jobbr.Server.JobRegistry;
+
+namespace InsightQuizzer.JobServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new JobbrBuilder();
+
+            //JobbrBuilder.AddInProcessExecution(config =>
+            //{
+            //    config.IsRuntimeWaitingForDebugger = true;
+            //});
+
+            builder.AddJobs(r =>
+            {
+                r.Define("QuizJob", "InsightQuizzer.JobServer.Jobs.QuizJob")
+                    .WithTrigger("* * * * *", noParallelExecution: true);
+            });
+
+            using (var server = builder.Create())
+            {
+                server.Start();
+
+                Console.ReadLine();
+
+                server.Stop();
+            }
+        }
+    }
+}
